00c7759a3d946cd1e22b245bbce2d6ec5e9fe789
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2024-03-30 14:13:39 -0300

    nueva version. gitignore

--- src/bin/lfp.rs
+++ src/bin/lfp.rs
@@ -1,6 +1,9 @@
+use std::env::args;
+
 use glotaran_converter_lib::run_lfp;
 
 
 fn main() {
-	let _otp = run_lfp("PER MOFM Air 450 750 nm emision.txt").unwrap();
+	let filename =  args().last().unwrap();
+	let _otp = run_lfp(&filename).unwrap();
 }
